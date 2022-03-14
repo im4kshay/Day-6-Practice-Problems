8a9f7862ce8ae38ba9859f1f0cd0ac01d11cba0a
im4kshay <73643584+im4kshay@users.noreply.github.com>
2022-03-15 02:27:12 +0530

    Write a program that computes a factorial of a number taken as input.

---
+++ factorial.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+#Write a program that compute Factorial of a number taken as input
+# 5 Factorial -5!= 1*2*3*4*5
+
+factorial=1
+read -p "Enter a number: " num
+
+for(( i=$num ; i>1 ; i-- ))
+do
+   factorial=$(( $factorial * $i ))
+done
+
+echo $factorial
